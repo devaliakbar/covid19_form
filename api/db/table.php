02a5883fe2f1479a8dd4cba4250038f67f5885d0
Ali <dev.aliakbar@gmail.com>
2020-05-05 03:11:53 +0530

    Added Structure for Quarantine Form

--- api/db/table.php
+++ api/db/table.php
@@ -12,6 +12,22 @@ if (mysqli_query($conn, $userInfoCreateQuery)) {
     echo "Error creating table: " . mysqli_error($conn);
 }
 
+$deleteAllUserQuery = 'DELETE FROM user_info';
+if (mysqli_query($conn, $deleteAllUserQuery)) {
+    echo "<br>Cleared Users successfully<br>";
+} else {
+    echo "Failed To Delete Old Users: " . mysqli_error($conn);
+}
+
+$addUserQuery = "INSERT INTO user_info(user_username, user_password)VALUES('admin', 'admin');";
+if (mysqli_query($conn, $addUserQuery)) {
+    echo "<br>Added Username : admin and Password : admin<br>";
+} else {
+    echo "Failed To Create Users: " . mysqli_error($conn);
+}
+
+//NORKA TABLE
+
 $personInfoCreateQuery = "CREATE TABLE person_info(
     _id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     organisation_name VARCHAR(50),
@@ -41,16 +57,110 @@ if (mysqli_query($conn, $personInfoCreateQuery)) {
     echo "Error creating table: " . mysqli_error($conn);
 }
 
-$deleteAllUserQuery = 'DELETE FROM user_info';
-if (mysqli_query($conn, $deleteAllUserQuery)) {
-    echo "<br>Cleared Users successfully<br>";
+//QUARINTINE TABLE
+
+$quarantineInfoCreateQuery = "CREATE TABLE quarantine_info(
+    _id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(50),
+    age INT,
+    sex BOOLEAN,
+    address VARCHAR(100),
+    district VARCHAR(50),
+    contact_number VARCHAR(50),
+    passport_number VARCHAR(50),
+    orgin_country VARCHAR(50),
+    orgin_state VARCHAR(50),
+    orgin_district VARCHAR(50),
+    phc_area VARCHAR(50),
+    phc_medical_officer_contact_number VARCHAR(50),
+    place_to_vist VARCHAR(50),
+    departure_date DATE,
+    arrival_date DATE,
+    date_of_information_received DATE,
+    source_of_information VARCHAR(50),
+    panchayat_ward_no VARCHAR(50),
+    source_of_contact_number VARCHAR(50),
+    observation_started_date DATE,
+    observation_end_date DATE,
+    current_health_status VARCHAR(50),
+    sample_to_test_taken BOOLEAN,
+    date_of_sample_taken DATE,
+    result VARCHAR(50),
+    travelled_with_positive_case BOOLEAN,
+    remark VARCHAR(500)
+);";
+
+if (mysqli_query($conn, $quarantineInfoCreateQuery)) {
+    echo "<br>Table Quarantine Info created successfully<br>";
 } else {
-    echo "Failed To Delete Old Users: " . mysqli_error($conn);
+    echo "Error creating table: " . mysqli_error($conn);
 }
 
-$addUserQuery = "INSERT INTO user_info(user_username, user_password)VALUES('admin', 'admin');";
-if (mysqli_query($conn, $addUserQuery)) {
-    echo "<br>Added Username : admin and Password : admin<br>";
+// FAMILY DETAIL
+
+$familyInfoCreateQuery = "CREATE TABLE family_info(
+    _id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    quarantine_id INT,
+    under_five INT,
+    five_to_ten INT,
+    ten_to_seventeen INT,
+    seventeen_to_fiftynine INT,
+    sixty_and_above INT,
+    details VARCHAR(500)
+);";
+
+if (mysqli_query($conn, $familyInfoCreateQuery)) {
+    echo "<br>Table Family Info created successfully<br>";
 } else {
-    echo "Failed To Create Users: " . mysqli_error($conn);
+    echo "Error creating table: " . mysqli_error($conn);
+}
+
+// VISITED PLACE
+
+$vistedPlaceInfoCreateQuery = "CREATE TABLE visited_place_info(
+    _id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    quarantine_id INT,
+    location_name VARCHAR(100),
+    lat VARCHAR(500),
+    lon VARCHAR(500)
+);";
+
+if (mysqli_query($conn, $vistedPlaceInfoCreateQuery)) {
+    echo "<br>Table Visited Place Info created successfully<br>";
+} else {
+    echo "Error creating table: " . mysqli_error($conn);
+}
+
+// PRIMARY CONTACT DETAIL
+
+$primaryContactInfoCreateQuery = "CREATE TABLE primary_contact_info(
+    _id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    quarantine_id INT,
+    name VARCHAR(100),
+    mobile_no VARCHAR(100),
+    location VARCHAR(500),
+    age INT
+);";
+
+if (mysqli_query($conn, $primaryContactInfoCreateQuery)) {
+    echo "<br>Table Primary Contact Info created successfully<br>";
+} else {
+    echo "Error creating table: " . mysqli_error($conn);
+}
+
+// SECONDARY CONTACT DETAIL
+
+$secondaryContactInfoCreateQuery = "CREATE TABLE secondary_contact_info(
+    _id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    quarantine_id INT,
+    name VARCHAR(100),
+    mobile_no VARCHAR(100),
+    location VARCHAR(500),
+    age INT
+);";
+
+if (mysqli_query($conn, $secondaryContactInfoCreateQuery)) {
+    echo "<br>Table Secondary Contact Info created successfully<br>";
+} else {
+    echo "Error creating table: " . mysqli_error($conn);
 }
